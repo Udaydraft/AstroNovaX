90ebe523bce42d25fb35ab9fe18ad328aae53ef4
Udaydraft <udaydraft@gmail.com>
2026-08-31 11:49:28 +0530

    feat: integrate Vercel Analytics into App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import Home from "./pages/Home";
+﻿import Home from "./pages/Home";
 import { ThemeProvider } from "./context/ThemeContext";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   return (
     <ThemeProvider>
       <Home />
+      <Analytics />
     </ThemeProvider>
   );
 }
